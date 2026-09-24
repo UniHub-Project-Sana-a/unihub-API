7c52db522e96bc77f48d89f6e6847757e5294408
Alaa-Hussein002 <ala.hussein002@gmail.com>
2026-09-24 19:58:44 +0300

    refactor: update PostgreSQL connection settings to use DATABASE_URL for improved configuration

--- config/database.php
+++ config/database.php
@@ -85,12 +85,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', parse_url(env('DATABASE_URL'), PHP_URL_HOST) ?: '127.0.0.1'),
+            'port' => env('DB_PORT', parse_url(env('DATABASE_URL'), PHP_URL_PORT) ?: '5432'),
+            'database' => env('DB_DATABASE', ltrim(parse_url(env('DATABASE_URL'), PHP_URL_PATH) ?: '', '/')),
+            'username' => env('DB_USERNAME', parse_url(env('DATABASE_URL'), PHP_URL_USER) ?: 'forge'),
+            'password' => env('DB_PASSWORD', parse_url(env('DATABASE_URL'), PHP_URL_PASS) ?: ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
